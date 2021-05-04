5d4949f7bc0864cafad6a7401b3dd99d86877c27
dejan-pejovic <81944397+dejan-pejovic@users.noreply.github.com>
2021-05-04 14:12:10 -0400

    Update cobrowse.js

--- cobrowse-webchat/cobrowse.js
+++ cobrowse-webchat/cobrowse.js
@@ -3,11 +3,11 @@
  * their specific installations.
  */
 var config = {
-  orgId: 'b969b710-80cf-4689-9bcf-e9f3befa8e05',
+  orgId: '8fc6d935-8073-46b7-8240-727f42969740',
   orgName: 'Startelecom',
   queueName: '',
   agentEmail: '',
-  cobrowseDeploymentKey: '8fc6d935-8073-46b7-8240-727f42969740'
+  cobrowseDeploymentKey: '8ef8eX9bN2Dzl8xBKPWnWvLvQYJdo119'
 };
 
 var webchatConfig = {
